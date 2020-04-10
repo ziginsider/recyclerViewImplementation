23546eedf1f78cd6d3d3e0959f3c89316357a986
Aliaksei Kisel <Aliaksei_Kisel@epam.com>
2020-04-10 12:39:21 +0300

    Add ViewBinding

--- app/src/main/java/io/ziginsider/github/MainActivity.kt
+++ app/src/main/java/io/ziginsider/github/MainActivity.kt
@@ -2,11 +2,20 @@ package io.ziginsider.github
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.ziginsider.github.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_main)
+
+        binding.listOfUsers.apply {
+            
+        }
     }
 }
